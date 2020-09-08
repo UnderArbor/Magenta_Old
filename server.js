5952168cc3937180b035b3842075dfcbe90cc3e7
UnderArbor <wavesmash6@gmail.com>
2020-09-08 16:04:39 -0700

    Traversey Changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db.js');
+const path = require('path');
 
 const app = express();
 
@@ -10,12 +11,21 @@ connectDB();
 
 // Init Middleware
 // app.use(express.json({ extended: false }));
-app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/deck', require('./routes/api/deck'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'prouction') {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 4000;
 
